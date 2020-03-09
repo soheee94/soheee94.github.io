9d31cb08ab099f1da4af47d4c9c0ca300bd96167
한소희 <hsh8616@naver.com>
2020-03-09 17:40:35 +0900

    헤더 로고 적용, 메뉴 버튼 보류

--- portfolio/src/component/Header.js
+++ portfolio/src/component/Header.js
@@ -1,8 +1,9 @@
 import React, { useState, useLayoutEffect } from "react";
-import styled, { css, keyframes } from "styled-components";
+import styled, { css } from "styled-components";
 import { useScroll, useHeader } from "../ScrollContext";
 import media from "../utils/media";
 import Menu from "./Menu";
+import logo from "../asset/logo.png";
 
 const ScrollAnimationStyles = css`
   visibility: visible;
@@ -13,7 +14,6 @@ const ScrollAnimationStyles = css`
 
 const Block = styled.header`
   background: ${props => props.theme.main};
-  color: ${props => props.theme.white};
   position: fixed;
   top: 0;
   left: 0;
@@ -25,38 +25,31 @@ const Block = styled.header`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  /* justify-content: space-between; */
   z-index: 2;
-  & > * {
-    padding: 60px;
-
-    ${media.small} {
-      padding: 25px 20px;
-    }
-  }
-
-  /* scroll animation (not mobile) */
-  @media (min-width: 1024px) {
-    ${props => {
-      if (props.scrollY > 3 && !props.openHeader) {
-        return css`
-          width: 176px;
-          ${MenuButton}, ${HeaderContent} {
-            visibility: hidden;
-            opacity: 0;
-            transition-delay: 0s;
-          }
-        `;
-      }
-    }}
-  }
-
-  /* mobile */
   ${media.medium} {
+    align-items: flex-start;
     width: 100%;
     position: relative;
     padding: 0;
+  }
+
+  ${props => {
+    if (props.scrollY > 3 && !props.openHeader) {
+      return css`
+        width: 132px;
+        ${HeaderContent} {
+          visibility: hidden;
+          opacity: 0;
+          transition-delay: 0s;
+        }
+      `;
+    }
+  }}
+  & > * {
+    padding: 3rem;
+    ${media.small} {
+      padding: 25px 20px;
+    }
   }
 `;
 const FloatingBlock = styled.div`
@@ -68,104 +61,17 @@ const FloatingBlock = styled.div`
   position: fixed;
   background: inherit;
   top: 0;
-  ${props =>
+  /* ${props =>
     props.openMenu &&
     css`
       background: none;
-    `}
-
-  /* hamburger menu test */
-  body {
-    background-color: #333;
-  }
+    `} */
 `;
 
 const Logo = styled.div`
-  width: 56px;
-  height: 56px;
-  min-width: 56px;
-  background: ${props => props.theme.white};
+  width: 48px;
+  height: 48px;
   cursor: pointer;
-`;
-
-const lineUp = keyframes`
-  0% {
-    width: 0%;
-  }
-  100% {
-    width: 100%;
-  }
-`;
-
-const MenuButton = styled.div`
-  ${ScrollAnimationStyles}
-  width: 26px;
-  height: 17px;
-  cursor: pointer;
-  position: relative;
-  &:before,
-  &:after,
-  span {
-    background-color: #fff;
-    content: "";
-    display: block;
-    height: 3px;
-    transition: all 200ms ease-in-out;
-  }
-  &:before {
-    margin-bottom: 11px;
-    animation: ${lineUp} ease 1s infinite 1s alternate;
-  }
-
-  span {
-    width: 100%;
-    position: absolute;
-    top: 7px;
-    animation: ${lineUp} ease 1.4s infinite 0.6s alternate;
-  }
-
-  &:after {
-    animation: ${lineUp} ease 1.8s infinite 0.2s alternate;
-  }
-
-  /* hover animation */
-  &:hover {
-    &:before,
-    &:after,
-    span {
-      background-color: ${props => props.theme.point};
-    }
-  }
-
-  ${media.medium} {
-    &:hover {
-      &:before,
-      &:after,
-      span {
-        background-color: white;
-      }
-    }
-  }
-
-  /* close button */
-  ${props =>
-    props.openMenu &&
-    css`
-      &:before,
-      &:after,
-      span {
-        animation: none;
-      }
-      &:before {
-        transform: translateY(7px) rotate(45deg);
-      }
-      &:after {
-        transform: translateY(-7px) rotate(-45deg);
-      }
-      span {
-        background: none !important;
-      }
-    `}
 `;
 
 const HeaderContent = styled.div`
@@ -179,44 +85,33 @@ const HeaderContent = styled.div`
 
 
   ${media.medium} {
-    padding-top: 176px;
+    padding-top: 150px;
     padding-bottom: 60px;
   }
   ${media.small} {
-    padding-top: 130px;
-    padding-bottom: 60px;
-    p {
-      padding: 0 20px;
-    }
+    padding-top: 80px;
+    /* padding-bottom: 60px; */
   }
 `;
-
-// const Footer = styled.div`
-//   ${ScrollAnimationStyles}
-//   z-index: 11;
-//   ${media.medium} {
-//     display: none;
-//   }
-// `;
 
 function Header() {
   const { scrollY } = useScroll();
   const { openHeader, setOpenHeader } = useHeader();
-  const [openMenu, setOpenMenu] = useState(false);
-  useLayoutEffect(() => {
-    const handleScroll = () => {
-      if (!openHeader) setOpenMenu(false);
-    };
+  // const [openMenu, setOpenMenu] = useState(false);
+  // useLayoutEffect(() => {
+  //   const handleScroll = () => {
+  //     if (!openHeader) setOpenMenu(false);
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  });
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // });
 
   const onMouseLeave = () => {
     setOpenHeader(false);
-    if (scrollY > 3) {
-      setOpenMenu(false);
-    }
+    // if (scrollY > 3) {
+    //   setOpenMenu(false);
+    // }
   };
   return (
     <Block
@@ -227,11 +122,10 @@ function Header() {
       }}
       onMouseLeave={onMouseLeave}
     >
-      <FloatingBlock openMenu={openMenu}>
-        <Logo />
-        <MenuButton onClick={() => setOpenMenu(!openMenu)} openMenu={openMenu}>
-          <span></span>
-        </MenuButton>
+      <FloatingBlock>
+        <Logo>
+          <img src={logo} alt="logo" />
+        </Logo>
       </FloatingBlock>
       <HeaderContent>
         <h1>
@@ -250,7 +144,7 @@ function Header() {
           디자인을 생각한 소스코드를 작성합니다.
         </p>
       </HeaderContent>
-      <Menu openMenu={openMenu} />
+      {/* <Menu openMenu={openMenu} /> */}
     </Block>
   );
 }
---
+++ portfolio/src/component/MenuButton.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const lineUp = keyframes`
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
+`;
+
+const MenuButtonBlock = styled.div`
+  ${ScrollAnimationStyles}
+  width: 26px;
+  height: 17px;
+  cursor: pointer;
+  position: relative;
+  &:before,
+  &:after,
+  span {
+    background-color: #fff;
+    content: "";
+    display: block;
+    height: 3px;
+    transition: all 200ms ease-in-out;
+  }
+  &:before {
+    margin-bottom: 11px;
+    animation: ${lineUp} ease 1s infinite 1s alternate;
+  }
+
+  span {
+    width: 100%;
+    position: absolute;
+    top: 7px;
+    animation: ${lineUp} ease 1.4s infinite 0.6s alternate;
+  }
+
+  &:after {
+    animation: ${lineUp} ease 1.8s infinite 0.2s alternate;
+  }
+
+  /* hover animation */
+  &:hover {
+    &:before,
+    &:after,
+    span {
+      background-color: ${props => props.theme.point};
+    }
+  }
+
+  ${media.medium} {
+    &:hover {
+      &:before,
+      &:after,
+      span {
+        background-color: white;
+      }
+    }
+  }
+
+  /* close button */
+  ${props =>
+    props.openMenu &&
+    css`
+      &:before,
+      &:after,
+      span {
+        animation: none;
+      }
+      &:before {
+        transform: translateY(7px) rotate(45deg);
+      }
+      &:after {
+        transform: translateY(-7px) rotate(-45deg);
+      }
+      span {
+        background: none !important;
+      }
+    `}
+`;
+
+function MenuButton({ openMenu, setOpenMenu }) {
+  return (
+    <MenuButtonBlock onClick={() => setOpenMenu(!openMenu)} openMenu={openMenu}>
+      <span></span>
+    </MenuButtonBlock>
+  );
+}
+
+export default MenuButton;
