e6f7df5e0426a6d96297483ab29de7d0644100d9
한소희 <hsh8616@naver.com>
2020-03-10 21:59:18 +0900

    main work container div 추가

--- portfolio/src/container/main/MainWorkContainer.js
+++ portfolio/src/container/main/MainWorkContainer.js
@@ -5,11 +5,11 @@ import data from "../../data";
 function MainWorkContainer() {
   const workList = data.Works;
   return (
-    <>
+    <div>
       {workList.map(({ title, summary, image }, index) => (
         <MainWork title={title} summary={summary} image={image} key={index} />
       ))}
-    </>
+    </div>
   );
 }
 
