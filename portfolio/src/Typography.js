7b7a9676acf75d52670c0f1f82e7f66113319657
soheee94 <hsh8616@naver.com>
2020-03-10 12:00:39 +0900

    폰트 사이즈 재조정

--- portfolio/src/Typography.js
+++ portfolio/src/Typography.js
@@ -1,36 +1,25 @@
 import React from "react";
 import styled from "styled-components";
-import media from "./utils/media";
-
-// 1920 기준
-// large title  : 46px
-// title 1 : 36px
-// p :22px
 
 const TypographyBlock = styled.div`
   h1 {
-    font-size: 2.5rem;
-    line-height: 3.4rem;
+    font-size: 2rem;
+    line-height: 2.91rem;
     letter-spacing: -0.065rem;
   }
-  /* h1 + p {
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-    letter-spacing: -0.055rem;
-  } */
   h2 {
-    font-size: 2rem;
-    line-height: 3.25rem;
+    font-size: 1.125rem;
+    line-height: 1.625rem;
     letter-spacing: -0.035rem;
     font-weight: 700;
   }
   h1 + p {
-    font-size: 1.375rem;
-    line-height: 2.25rem;
+    font-size: 1rem;
+    line-height: 1.6rem;
     letter-spacing: -0.055rem;
   }
   p {
-    font-size: 1.125rem;
+    font-size: 0.875rem;
     line-height: 2.25rem;
     letter-spacing: -0.055rem;
   }
