1c578621fe9a0ac6621b21f32da63b7232ea4e6a
soheee94 <hsh8616@naver.com>
2020-03-12 18:08:58 +0900

    icons text 추가

--- portfolio/src/component/common/FloatingLogo.js
+++ portfolio/src/component/common/FloatingLogo.js
@@ -20,8 +20,14 @@ function FloatingLogo() {
         onMouseLeave={() => setImageSrc(logo)}
       />
       <div className="icons">
-        <IoLogoGithub onClick={() => window.open("https://github.com/soheee94")} />
-        <IoIosMail onClick={() => (window.location.href = "mailto:hsh8616@naver.com")} />
+        <div onClick={() => window.open("https://github.com/soheee94")}>
+          <IoLogoGithub />
+          <span>Github</span>
+        </div>
+        <div onClick={() => (window.location.href = "mailto:hsh8616@naver.com")}>
+          <IoIosMail />
+          <span>Mail</span>
+        </div>
       </div>
     </FloatingLogoBlock>
   );
@@ -38,13 +44,15 @@ const FloatingLogoBlock = styled.div`
   top: 0;
   left: 0;
   padding: 3rem;
-  color: white;
+  height: calc(6rem + 48px);
 
   ${media.xlarge} {
     padding: 2rem;
+    height: calc(4rem + 48px);
   }
   ${media.medium} {
     padding: 25px 2rem;
+    height: calc(50px + 48px);
   }
 
   img {
@@ -57,16 +65,43 @@ const FloatingLogoBlock = styled.div`
     opacity: 1;
     transition: all 0.15s ease;
     transition-delay: 0.5s;
-  }
+    color: #796b62;
+    display: flex;
+    align-items: center;
 
-  svg {
-    font-size: 1.25rem;
-    margin-left: 1rem;
-    font-weight: bold;
-    cursor: pointer;
-    transition: all 0.25s ease;
-    &:hover {
-      color: ${palette.point};
+    div {
+      padding: 0 1rem;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      transition: all 0.25s ease;
+      position: relative;
+      &:last-of-type {
+        &:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          height: 50%;
+          width: 1px;
+          border-left: 1px solid #796b62;
+          margin: 0 auto;
+        }
+        padding-right: 0;
+      }
+
+      &:hover {
+        color: ${palette.point};
+      }
+
+      svg {
+        font-size: 1.625rem;
+        margin-right: 0.25rem;
+      }
+
+      span {
+        font-size: 0.875rem;
+        letter-spacing: -0.03rem;
+      }
     }
   }
 `;
